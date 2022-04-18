435031213cc32c8b598d214d218270367532ad92
David Luevano Alvarado <david@luevano.xyz>
2022-04-17 23:36:56 -0600

    add typing to formatter

--- src/pyssg/per_level_formatter.py
+++ src/pyssg/per_level_formatter.py
@@ -4,14 +4,14 @@ from logging import Formatter
 
 class PerLevelFormatter(logging.Formatter):
     # colors for the terminal in ansi
-    yellow = "\x1b[33m"
-    red = "\x1b[31m"
-    bold_red = "\x1b[31;1m"
-    reset = "\x1b[0m"
+    yellow: str = "\x1b[33m"
+    red: str = "\x1b[31m"
+    bold_red: str = "\x1b[31;1m"
+    reset: str = "\x1b[0m"
 
-    DATE_FMT = '%Y-%m-%d %H:%M:%S'
-    COMMON_FMT = '[%(levelname)s] [%(module)s:%(funcName)s:%(lineno)d]: %(message)s'
-    FORMATS = {
+    DATE_FMT: str = '%Y-%m-%d %H:%M:%S'
+    COMMON_FMT: str = '[%(levelname)s] [%(module)s:%(funcName)s:%(lineno)d]: %(message)s'
+    FORMATS: dict[int, str] = {
         logging.DEBUG: COMMON_FMT,
         logging.INFO: '%(message)s',
         logging.WARNING: f'{yellow}{COMMON_FMT}{reset}',
